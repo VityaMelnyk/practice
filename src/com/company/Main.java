3af36fd7b7d03feb849543f4120010c216d86f1e
Viki <vityamozer@gmail.com>
2021-02-18 16:45:50 +0200

    Сдвиг всех чисел в лево

---
+++ src/com/company/Main.java
@@ -0,0 +1,39 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	int[] array = new int[]{10, 20, 30, 40, 50};
+	int buffer = array [0];
+        for (int i = 0; i < array.length; i++) {
+            if (i == array.length-1){
+                array[i] = buffer;
+            }else {
+                array[i] = array[i+1];
+            }
+        }
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(" " + array[i]);
+        }
+        // 10 20 30 40 50
+        //После первого цикла 20 20 30 40 50
+        //После второго цикла 20 30 30 40 50
+        //После третього цикла 20 30 40 40 50
+        //После четвертого цикла 20 30 40 50 50
+        //На пятом цикле пятому елементу даем БУФФЕР
+
+        buffer = array[array.length-1];
+
+        for (int i = array.length-1; i >= 0; i--) {
+            if (i == 0) {
+                array[i] = buffer;
+            } else {
+                array[i] = array[i - 1];
+            }
+        }
+        System.out.println();
+            for (int i = 0; i < array.length; i++) {
+                System.out.print(" " + array[i]);
+            }
+    }
+}
